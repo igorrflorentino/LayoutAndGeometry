7044395d017a591b1e06c97ec0cd68600b42bb74
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-08-01 16:23:17 -0300

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Igor Florentino on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
